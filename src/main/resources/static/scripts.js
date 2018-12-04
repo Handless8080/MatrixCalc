35d005c633e01e902c8438e761c074413dce7983
Handless8080 <vacya9@gmail.com>
2018-12-04 18:53:51 +0300

    Initial commit

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn-matr-count-more').addEventListener('click', createTable);
     document.getElementById('btn-matr-count-less').addEventListener('click', deleteTable);
 
-    document.getElementById('btn-rows-count-more0').addEventListener('click', createRow);
-    document.getElementById('btn-rows-count-less0').addEventListener('click', deleteRow);
+    document.getElementById('btn-rows-count-more0').addEventListener('click', createRows);
+    document.getElementById('btn-rows-count-less0').addEventListener('click', deleteRows);
 
-    document.getElementById('btn-cols-count-more0').addEventListener('click', createColumn);
-    document.getElementById('btn-cols-count-less0').addEventListener('click', deleteColumn);
+    document.getElementById('btn-cols-count-more0').addEventListener('click', createColumns);
+    document.getElementById('btn-cols-count-less0').addEventListener('click', deleteColumns);
 });
 
 function getOperator() {
@@ -50,6 +50,7 @@ function createSizeInput(id, i) {
     var tr = document.getElementById(id + 's');
 
     var select = document.createElement('td');
+    select.id = id == "col" ? "ctd" + i : "rtd" + i;
     var div = document.createElement('div');
     div.classList.add('input-group');
     div.style.minWidth = "91px";
@@ -95,6 +96,17 @@ function createSizeInput(id, i) {
 
     select.appendChild(div);
     tr.appendChild(select);
+
+    if (id == "col") {
+        btnLess.addEventListener('click', deleteColumns);
+        btnMore.addEventListener('click', createColumns);
+    } else {
+        btnLess.addEventListener('click', deleteRows);
+        btnMore.addEventListener('click', createRows);
+    }
+
+    var col = document.getElementById('col' + (parseInt(i, 10) - 1)).value;
+    input.value = col;
 }
 
 function setOperator() {
@@ -105,6 +117,20 @@ function setOperator() {
 
     select.innerHTML = operator;
     var char = getOperator();
+
+    while (header.firstChild) {
+        header.removeChild(header.firstChild);
+    }
+
+    for (var i = 1; i < tables; i++) {
+        var colSize = document.getElementById('ctd' + i);
+        var rowSize = document.getElementById('rtd' + i);
+
+        if (colSize && rowSize) {
+            colSize.remove();
+            rowSize.remove();
+        }
+    }
 
     if (char == "*") {
         var th = document.createElement('th');
@@ -117,10 +143,10 @@ function setOperator() {
         number1.innerHTML = "1";
         header.appendChild(number1);
 
-        for (var i = 2; i <= tables; i++) {
+        for (var i = 1; i < tables; i++) {
             var number = document.createElement('th');
             number.scope = "col";
-            number.innerHTML = i;
+            number.innerHTML = parseInt(i, 10) + 1;
             header.appendChild(number);
 
             createSizeInput('col', i);
@@ -136,14 +162,28 @@ function setOperator() {
         var op = document.getElementById('op' + i);
         op.innerHTML = char;
     }
-};
+}
 
 function createTable() {
-    var col = document.getElementById('col0').value;
-    var row = document.getElementById('row0').value;
     var matrCount = document.getElementById('matr-count').value;
+    if (getOperator() == "*") {
+        var col = document.getElementById('col' + (parseInt(matrCount, 10) - 1)).value;
+        var row = 1;
+    } else {
+        var col = document.getElementById('col0').value;
+        var row = document.getElementById('row0').value;
+    }
 
     if (matrCount < 10) {
+        if (getOperator() != "+" && getOperator() != "-") {
+            createSizeInput('col', matrCount);
+            createSizeInput('row', matrCount);
+
+            var number = document.createElement('th');
+            number.scope = "col";
+            number.innerHTML = parseInt(matrCount, 10) + 1;
+            header.appendChild(number);
+        }
         document.getElementById('matr-count').value++;
         document.getElementById('op' + (parseInt(matrCount, 10) - 1)).innerHTML = getOperator();
 
@@ -168,11 +208,19 @@ function createTable() {
 
 function deleteTable() {
     var matrCount = document.getElementById('matr-count').value;
+    var header = document.getElementById('header');
+    var cols = document.getElementById('cols');
+    var rows = document.getElementById('rows');
 
     if (matrCount > 1) {
         document.getElementById('matr-count').value--;
         document.getElementById('op' + (parseInt(matrCount, 10) - 2)).innerHTML = "";
 
+        if (header.lastChild && cols.lastChild && rows.lastChild) {
+            header.removeChild(header.lastChild);
+            cols.removeChild(cols.lastChild);
+            rows.removeChild(rows.lastChild);
+        }
         var d = document.getElementById('inp' + (parseInt(matrCount, 10) - 1));
         while (d.firstChild) {
             d.removeChild(d.firstChild);
@@ -180,92 +228,199 @@ function deleteTable() {
     }
 }
 
-function createRow() {
-    var rowCount = document.getElementById('row0');
+function createRows() {
+    var id = this.id;
+    var index = id[parseInt(id.length, 10) - 1];
+    var rowCount = document.getElementById('row' + index);
 
-    var col = document.getElementById('col0').value;
     var row = rowCount.value;
     var matrCount = document.getElementById('matr-count').value;
 
     if (row < 10) {
         rowCount.value++;
 
-        for (var t = 0; t < matrCount; t++) {
-            var d = document.getElementById('inp' + t)
-            var div = document.createElement('div');
-            div.id = "inp" + t + row;
-            div.classList.add('d-inline-flex');
-            div.classList.add('flex-row');
-            d.appendChild(div);
-
-            for (i = 0; i < col; i++) {
-                var input = document.createElement('input');
-                input.type = "text";
-                input.id = "number" + t + row + i;
-                input.style.width = "50px";
-                div.appendChild(input);
-            }
+        switch (getOperator()) {
+            case "+":
+            case "-":
+                for (var t = 0; t < matrCount; t++) {
+                    createRow(t);
+                }
+                break;
+            case "*":
+                if (index != 0) {
+                    var columnCount = document.getElementById('col' + (parseInt(index, 10) - 1));
+                    columnCount.value++;
+                    createRow(index);
+                    createColumn(parseInt(index, 10) - 1);
+                }
+                break;
+            default:
+                createRow(index);
         }
     }
 }
 
-function deleteRow() {
-    var rowCount = document.getElementById('row0');
+function deleteRows() {
+    var id = this.id;
+    var index = id[parseInt(id.length, 10) - 1];
+    var rowCount = document.getElementById('row' + index);
 
     var matrCount = document.getElementById('matr-count').value;
 
     if (rowCount.value > 1) {
         rowCount.value--;
 
-        for (var t = 0; t < matrCount; t++) {
-            var div = document.getElementById('inp' + t + rowCount.value);
-            div.remove();
+        switch (getOperator()) {
+            case "+":
+            case "-":
+                for (var t = 0; t < matrCount; t++) {
+                    deleteRow(t);
+                }
+                break;
+            case "*":
+                if (index != 0) {
+                    var columnCount = document.getElementById('col' + (parseInt(index, 10) - 1));
+                    columnCount.value--;
+                    deleteRow(index);
+                    deleteColumn(parseInt(index, 10) - 1);
+                }
+                break;
+            default:
+                deleteRow(index);
         }
     }
 }
 
-function createColumn() {
-    var colCount = document.getElementById('col0');
+function createColumns() {
+    var id = this.id;
+    var index = id[parseInt(id.length, 10) - 1];
+    var colCount = document.getElementById('col' + index);
 
     var col = colCount.value;
-    var row = document.getElementById('row0').value;
     var matrCount = document.getElementById('matr-count').value;
 
     if (col < 10) {
         colCount.value++;
 
-        for (var t = 0; t < matrCount; t++) {
-            var d = document.getElementById('inp' + t)
-
-            for (var i = 0; i < row; i++) {
-                var div = document.getElementById('inp' + t + i);
-                var input = document.createElement('input');
-                input.type = "text";
-                input.id = "number" + t + i + col;
-                input.style.width = "50px";
-                div.appendChild(input);
-            }
+        switch (getOperator()) {
+            case "+":
+            case "-":
+                for (var t = 0; t < matrCount; t++) {
+                    createColumn(t);
+                }
+                break;
+            case "*":
+                if (parseInt(index, 10) + 1 != matrCount) {
+                    var rowCount = document.getElementById('row' + (parseInt(index, 10) + 1));
+                    rowCount.value++;
+                    createColumn(index);
+                    createRow(parseInt(index, 10) + 1);
+                }
+                break;
+            default:
+                createColumn(index);
         }
     }
 }
 
-function deleteColumn() {
-    var colCount = document.getElementById('col0');
+function deleteColumns() {
+    var id = this.id;
+    var index = id[parseInt(id.length, 10) - 1];
+    var colCount = document.getElementById('col' + index);
 
     var col = colCount.value;
-    var row = document.getElementById('row0').value;
     var matrCount = document.getElementById('matr-count').value;
 
     if (col > 1) {
         colCount.value--;
 
-        for (var t = 0; t < matrCount; t++) {
-            var d = document.getElementById('inp' + t)
-
-            for (var i = 0; i < row; i++) {
-                var input = document.getElementById('number' + t + i + colCount.value);
-                input.remove();
-            }
+        switch (getOperator()) {
+            case "+":
+            case "-":
+                for (var t = 0; t < matrCount; t++) {
+                    deleteColumn(t);
+                }
+                break;
+            case "*":
+                if (parseInt(index, 10) + 1 != matrCount) {
+                    var rowCount = document.getElementById('col' + (parseInt(index, 10) + 1));
+                    rowCount.value--;
+                    deleteColumn(index);
+                    deleteRow(parseInt(index, 10) + 1);
+                }
+                break;
+            default:
+                deleteRow(index);
         }
+    }
+}
+
+function createRow(t) {
+    if (getOperator() == "+" || getOperator() == "-") {
+        var col = document.getElementById('col0').value;
+        var row = document.getElementById('row0').value;
+    } else {
+        var col = document.getElementById('col' + t).value;
+        var row = document.getElementById('row' + t).value;
+    }
+
+    var d = document.getElementById('inp' + t)
+    var div = document.createElement('div');
+    div.id = "inp" + t + (parseInt(row, 10) - 1);
+    div.classList.add('d-inline-flex');
+    div.classList.add('flex-row');
+    d.appendChild(div);
+
+    for (i = 0; i < col; i++) {
+        var input = document.createElement('input');
+        input.type = "text";
+        input.id = "number" + t + (parseInt(row, 10) - 1) + i;
+        input.style.width = "50px";
+        div.appendChild(input);
+    }
+}
+
+function createColumn(t) {
+    if (getOperator() == "+" || getOperator() == "-") {
+        var col = document.getElementById('col0').value;
+        var row = document.getElementById('row0').value;
+    } else {
+        var col = document.getElementById('col' + t).value;
+        var row = document.getElementById('row' + t).value;
+    }
+
+    for (var i = 0; i < row; i++) {
+        var div = document.getElementById('inp' + t + i);
+        var input = document.createElement('input');
+        input.type = "text";
+        input.id = "number" + t + i + (parseInt(col, 10) - 1);
+        input.style.width = "50px";
+        div.appendChild(input);
+    }
+}
+
+function deleteRow(t) {
+    if (getOperator() == "+" || getOperator() == "-") {
+        var row = document.getElementById('row0').value;
+    } else {
+        var row = document.getElementById('row' + t).value;
+    }
+
+    var div = document.getElementById('inp' + t + parseInt(row, 10));
+    div.remove();
+}
+
+function deleteColumn(t) {
+    if (getOperator() == "+" || getOperator() == "-") {
+        var col = document.getElementById('col0').value;
+        var row = document.getElementById('row0').value;
+    } else {
+        var col = document.getElementById('col' + t).value;
+        var row = document.getElementById('row' + t).value;
+    }
+
+    for (var i = 0; i < row; i++) {
+        var input = document.getElementById('number' + t + i + parseInt(col, 10));
+        input.remove();
     }
 }
